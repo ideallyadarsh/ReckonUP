ebb98dd49d4fb6f0636995a279489018b96f9ccd
Adarsh Shukla <50777364+ideallyadarsh@users.noreply.github.com>
2020-04-16 12:05:26 +0530

    Routes inserted

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New page"),
+
+      ),
+      body: Text("Welcome to the new page"),
+    );
+  }
+}
