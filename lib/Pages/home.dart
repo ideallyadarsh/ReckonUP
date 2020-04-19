4991b0200593077ca607f23275e7494493a32c05
Vishal Kumar Singh <01vksingh@gmail.com>
2020-04-19 18:31:36 +0530

    changes

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,31 +1,64 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+//import 'streambuilder_firestore.dart';
 class Home extends StatelessWidget {
   const Home({Key key, this.user}) : super(key: key);
   final FirebaseUser user;
+  Widget _buildList(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Text(document['Age']),
+      subtitle: Text(document['Name']),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance.collection('users').document(user.uid).snapshots(),
-        builder: (BuildContext context,AsyncSnapshot<DocumentSnapshot> snapshot){
-          if(snapshot.hasError){
-            return Text('error');
-          }
-          if (snapshot.hasData && snapshot.data.data != null){
-          switch(snapshot.connectionState){
-            case ConnectionState.waiting:return Text("Loading");
-            default:
-              return Text(snapshot.data['name']);
-          }
-          };
-        },
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('users').snapshots(),
+        // ignore: missing_return
+        builder: (context, snapshot) {
+    if (!snapshot.hasData) {
+    return Text("Loading..");
+    }
+    return ListView.builder(
+      itemExtent: 80.0,
+      itemCount: 2,
+      itemBuilder: (context, index) {
+        return _buildList(context, snapshot.data.documents[index]);
+      },
+    );
+    },
       ),
     );
 
   }
 }
+
+
+
+//
+//
+//
+//    body: StreamBuilder(
+//      stream: Firestore.instance.collection('posts').snapshots(),
+//      //print an integer every 2secs, 10 times
+//      builder: (context, snapshot) {
+//        if (!snapshot.hasData) {
+//          return Text("Loading..");
+//        }
+//        return ListView.builder(
+//          itemExtent: 80.0,
+//          itemCount: 2,
+//          itemBuilder: (context, index) {
+//            return _buildList(context, snapshot.data.documents[index]);
+//          },
+//        );
+//      },
+//    ),
+
